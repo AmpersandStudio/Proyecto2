317297df438555199e288dd6cd38b1298fa8ca3c
Juan Ruiz <juarui02@ucm.es>
2018-02-21 21:47:46 +0100

    cambiadas las tools a 2017

--- Proyecto/Clases y Estados/Game.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-//En esta clase correrá el bucle principal que llamará a handle event, render y update del estado en el que estemos. 
-class Game
-{
-public:
-	Game();
-	~Game();
-};
-
