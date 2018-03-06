aa0ba2525df49ce88849c0abfcff0bd11f4531da
Juan Ruiz <juarui02@ucm.es>
2018-03-06 17:53:50 +0100

    hecho estado de transicion a combate

--- Proyecto/RedBrickSky/RedBrickSky/DiscretMovementIAComponent.cpp
+++ Proyecto/RedBrickSky/RedBrickSky/DiscretMovementIAComponent.cpp
@@ -1,9 +1,10 @@
 #include "DiscretMovementIAComponent.h"
+#include "StateMachine.h"
+#include "BattleState.h"
 
 DiscretMovementIAComponent::DiscretMovementIAComponent()
 {
 }
-
 
 DiscretMovementIAComponent::~DiscretMovementIAComponent()
 {
@@ -13,7 +14,20 @@ void DiscretMovementIAComponent::update(GameObject * o) {
 	int screenW = o->getGame()->getWinWidth();
 	int screenH = o->getGame()->getWinHeight();
 	Vector2D pos = o->getPosition();
-	pos.setX(pos.getX() + 1);
+
+	int rows = o->getGame()->getWinHeight() / o->getWidth();
+	
+	pos.setX((pos.getX()) + 1);
 	if (pos.getX()*o->getWidth() < screenW)
 		o->setPosition(pos);
+	else if (pos.getX()*o->getWidth() >= screenW && pos.getY()*o->getHeight() < screenH)
+	{
+		pos.setX(0);
+		pos.setY(pos.getY() + 1);
+		o->setPosition(pos);
+	}
+	else {
+		StateMachine* sm = o->getGame()->getStateMachine();
+		sm->changeState(new BattleState(o->getGame()));
+	}
 }
