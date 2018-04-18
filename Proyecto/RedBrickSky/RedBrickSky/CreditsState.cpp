63e425b82a8ad7c5876fe7323177bec53207b3b4
nestorcabrero <nestor.cabrero@ucm.es>
2018-04-19 00:04:54 +0200

    Added a Credit State
    Added Right Chalk ttf font.
    Added blackboard image.
    Added logo image.
    Added credits music.
    Changed initial state in Game to test Credit State.

--- Proyecto/RedBrickSky/RedBrickSky/CreditsState.cpp
+++ Proyecto/RedBrickSky/RedBrickSky/CreditsState.cpp
@@ -1,9 +1,99 @@
 #include "CreditsState.h"
+#include "TextureManager.h"
+#include "Game.h"
+#include "SoundManager.h"
 
 CreditsState::CreditsState()
 {
+	SoundManager::Instance()->stopMusic();
+
+	timeStart_ = SDL_GetTicks();
+	timeDisplayInterval_ = 4500;
+	timeTitleInterval_ = 7000;
+	timeNameInterval_ = 6000;
+
+	speed_ = 0.9f;
+	position_ = (TheGame::Instance()->getWinHeight() / 2) - (TheTextureManager::Instance()->getHeight("logo") / 2);
+	alphaFactor_ = 1;
+	alpha_ = 255;
+
+	names_.push("MARTIN AMECHAZURRA");
+	names_.push("NURIA BANGO");
+	names_.push("NESTOR CABRERO");
+	names_.push("JAVIER CORDERO");
+	names_.push("RAUL FERNANDEZ");
+	names_.push("PABLO MARTIN");
+	names_.push("AARON REBOREDO");
+	names_.push("JUAN RUIZ");
+	names_.push("DEVELOPED BY &STUDIO");
+	names_.push("THANKS FOR PLAYING!");
+
+	displayShown_ = false;
+	titleShown_ = false;
+	titleMove_ = false;
+	nameShown_ = false;
+
+	SoundManager::Instance()->playMusic("credits", 0);
 }
 
 CreditsState::~CreditsState()
 {
+}
+
+bool CreditsState::handleEvent(const SDL_Event & event)
+{
+	return GameState::handleEvent(event);
+}
+
+void CreditsState::update()
+{
+	if (!displayShown_ && ((timeStart_ + timeDisplayInterval_) <= SDL_GetTicks()))
+	{
+		displayShown_ = true;
+		titleShown_ = true;
+		timeStart_ = SDL_GetTicks();
+	}
+
+	if (titleShown_ && !titleMove_ && ((timeStart_ + timeTitleInterval_) <= SDL_GetTicks()))
+	{
+		titleMove_ = true;
+	}
+
+	if (titleMove_)
+	{
+		position_ -= speed_;
+		if (position_ < ((TheGame::Instance()->getWinHeight() / 2) - (TheTextureManager::Instance()->getHeight("logo") / 2) - TheTextureManager::Instance()->getHeight("logo")))
+		{
+			titleShown_ = false;
+			titleMove_ = false;
+			nameShown_ = true;
+			timeStart_ = SDL_GetTicks();
+		}
+	}
+
+	if (displayShown_ && !titleShown_ && ((timeStart_ + timeNameInterval_) <= SDL_GetTicks()))
+	{
+		if(!names_.empty()) names_.pop();
+		timeStart_ = SDL_GetTicks();
+	}
+
+}
+
+void CreditsState::render()
+{
+	if (displayShown_ && !names_.empty())
+	{
+		TheTextureManager::Instance()->drawFull("blackboard", 0, 0, TheGame::Instance()->getWinWidth(), TheGame::Instance()->getWinHeight(), TheGame::Instance()->getRenderer(), 0, alpha_);
+	}
+
+	if (titleShown_)
+	{
+		TheTextureManager::Instance()->drawFrame("logo", (TheGame::Instance()->getWinWidth() / 2) - (TheTextureManager::Instance()->getWidth("logo") / 2),
+			position_, TheTextureManager::Instance()->getWidth("logo"), TheTextureManager::Instance()->getHeight("logo"), 0, 0, TheGame::Instance()->getRenderer(), 0, alpha_);
+	}
+
+	if (nameShown_)
+	{
+		if (!names_.empty()) TheTextureManager::Instance()->drawText(names_.front(), TextureManager::CHALK24, { 255, 255, 255, alpha_ }, 230, 290, TheGame::Instance()->getRenderer());
+	}
 }
--- Proyecto/RedBrickSky/RedBrickSky/CreditsState.h
+++ Proyecto/RedBrickSky/RedBrickSky/CreditsState.h
@@ -1,12 +1,36 @@
 #pragma once
+
 #include "GameState.h"
 #include "checkML.h"
 
-class CreditsState :
-	public GameState
+#include <queue>
+#include <string>
+
+class CreditsState : public GameState
 {
 public:
 	CreditsState();
 	virtual ~CreditsState();
+
+	bool handleEvent(const SDL_Event & event);
+	virtual void update();
+	virtual void render();
+
+private:
+	Uint32 timeStart_;
+	Uint32 timeNameInterval_;
+	Uint32 timeDisplayInterval_;
+	Uint32 timeTitleInterval_;
+
+	float position_;
+	float speed_;
+	Uint8 alpha_;
+	int alphaFactor_;
+	std::queue<std::string> names_;
+
+	bool displayShown_;
+	bool titleShown_;
+	bool titleMove_;
+	bool nameShown_;
 };
 
